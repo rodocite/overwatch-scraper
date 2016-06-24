3c2a0098844d08c5c6131ed6e03c32f12cbc2f0e
Rodolfo Yabut <rodolfoyabut@gmail.com>
2016-06-23 18:51:50 -0700

    Use wikia as source for game data
    - Wikia API for accessing character data in lieu of scraping from playoverwatch.com
    - Added type column for each hero
    - Removed descriptions for now

--- scrape-overwatch.js
+++ scrape-overwatch.js
@@ -1,20 +1,26 @@
 const request = require('request')
-const cheerio = require('cheerio')
-const Hero = require('./models/Hero');
+const Hero = require('./models/Hero')
 
-request('https://playoverwatch.com', (error, response, html) => {
+request('http://overwatch.wikia.com/api/v1/Navigation/Data', (error, response) => {
   if (!error && response.statusCode === 200) {
-    const $ = cheerio.load(html)
+    const data = JSON.parse(response.body).navigation.wiki
+    const heroes = data[1].children
+    let flattenedHeroes = [];
 
-    $('li.u-inline-block').each((i, portrait) => {
-      const attribs = portrait.children[0].attribs
-      const heroName = attribs['data-hero-name']
-      const heroDescription = attribs['data-hero-description']
-
-      Hero.create({
-        name: heroName,
-        description: heroDescription 
+    heroes.forEach(type => {
+      type.children.forEach(hero => {
+        flattenedHeroes.push({
+          name: hero.text,
+          type: type.text
+        })
       })
+    })
+
+    flattenedHeroes.forEach(hero => {
+      const name = hero.name
+      const type = hero.type
+
+      Hero.create({ name, type })
     })
   }
 })
